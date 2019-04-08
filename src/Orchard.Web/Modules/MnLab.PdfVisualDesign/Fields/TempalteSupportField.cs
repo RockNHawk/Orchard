049ea07999486d0c3bdbd3b356b41ef30addb871
h <h@h.com>
2019-04-08 16:32:36 +0800

    add tempalesupport field

---
+++ src/Orchard.Web/Modules/MnLab.PdfVisualDesign/Fields/TempalteSupportField.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.ContentManagement;
+using System.Collections.Generic;
+
+namespace MnLab.PdfVisualDesign.Fields
+{
+    public class TempalteSupportField : ContentField
+    {
+        public string HTML
+        {
+            get
+            {
+                return Storage.Get<string>("HTML");
+            }
+            set
+            {
+                Storage.Set<string>("HTML", value);
+            }
+        }
+    }
+}
